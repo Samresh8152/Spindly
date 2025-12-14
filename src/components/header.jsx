070830d5d36fda66c34ebb067294a00421108621
Samresh8152 <samreshc31@gmail.com>
2025-12-14 15:41:15 +0530

    database sucess user

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { LayoutDashboard, PenBox } from 'lucide-react';
-const Header = () => {
+import { checkUser } from '@/lib/checkUser';
+
+const Header = async() => {
+    await checkUser();
   return (
     <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
 
