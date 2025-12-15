636366247769e2f6ab6ebf87a031337ea1417673
Samresh8152 <samreshc31@gmail.com>
2025-12-15 19:39:12 +0530

    hio

---
+++ src/app/(main)/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='px-5'>  
+        
+            <h1 className=' text-6xl font-bold gradient-title'> Dashboard </h1>
+        
+    </div>
+  )
+}
+
+export default Dashboard
