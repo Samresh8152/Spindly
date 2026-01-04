e3e9c678ab11a3ad4b1c8620028536f8a3df5923
Samresh8152 <samreshc31@gmail.com>
2026-01-05 04:07:21 +0530

    create account sucess

--- src/components/create-account-drawer.jsx
+++ src/components/create-account-drawer.jsx
@@ -16,6 +16,10 @@ import { Input } from './ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 import { Switch } from './ui/switch';
 import { Button } from './ui/button';
+import useFetch from '../../hooks/use-fetch';
+import { createAccount } from '../../actions/dashboard';
+import { Loader2 } from 'lucide-react';
+import { toast } from 'sonner';
 const CreateAccountDrawer = ({children}) => {
     const [open , setOpen] = useState(false);
 
@@ -30,8 +34,26 @@ const CreateAccountDrawer = ({children}) => {
         }
     })
 
+
+    const {data:newAccount ,error,fn : createAccountFn,loading:createAccountLoading } =  useFetch(createAccount)
+
+    useEffect(() =>{
+
+        if(newAccount && !createAccountLoading){
+            toast.success("Account created successfully")
+            reset();
+            setOpen(false);
+        }
+    },[createAccountLoading,newAccount] )
+
+    useEffect(() =>{
+        if(error){
+            toast.error("Failed to create account. Please try again.")
+        }
+    } , [error] )
+
     const onSubmit = async(data) => {
-    console.log(data);
+    createAccountFn(data);
     };
 
   return (
@@ -60,7 +82,7 @@ const CreateAccountDrawer = ({children}) => {
   </SelectTrigger>
   <SelectContent>
     <SelectItem value="CURRENT">Current</SelectItem>
-    <SelectItem value="SAVING">Saving</SelectItem>
+    <SelectItem value="SAVINGS">Saving</SelectItem>
     
   </SelectContent>
 </Select>
@@ -93,7 +115,7 @@ const CreateAccountDrawer = ({children}) => {
                 <Button type='button' variant = ' outline ' className="flex-1" > Cancel </Button>
            
             </DrawerClose>
-                 <Button  type='submit' className='flex-1' >Create account</Button>
+                 <Button  type='submit' className='flex-1'disabled={createAccountLoading} > { createAccountLoading ?(<> <Loader2 className='mr-2 h-4 w-4 animate-spin' />"Creating.."</>):( "Create account") } </Button>
         </div>
 
         </form>
